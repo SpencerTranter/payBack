f3db5e093e5c353216bf23f0e7537d484656fce1
Anthony Morris <anthony@amorrissound.com>
2016-09-17 00:05:08 +0000

    Write all payBack.js functions

--- payBack.js
+++ payBack.js
@@ -8,3 +8,38 @@ exports.insertIOU = function(db, iou, cb) {
     return cb(null, result);
   });
 }
+
+exports.updateIOU = function(db, iou, cb) {
+  db.collection('ious').updateOne(
+    { '_id': iou.id },
+    {
+      $set: {
+        title: iou.title,
+        description: iou.description,
+        dateDue: iou.dateDue
+      }
+    }, (err, result) => {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, result);
+  });
+}
+
+exports.deleteURL = function(db, iou, cb) {
+  db.collection('ious').remove({ '_id': iou.id }, (err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(null, result);
+  });
+}
+
+exports.getURLs = function(db, cb) {
+  db.collection('ious').find().toArray((err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(null, result);
+  });
+}
